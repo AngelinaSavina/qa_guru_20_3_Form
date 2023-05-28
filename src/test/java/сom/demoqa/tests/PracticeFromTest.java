106b5134ba0614df451cc003a0a9904b6ce9f20a
angelinasavina <savinaangelina60@gmail.com>
2023-05-28 21:03:06 +0300

    Added Test for PracticeForm Page

---
+++ src/test/java/сom/demoqa/tests/PracticeFromTest.java
@@ -0,0 +1,79 @@
+package сom.demoqa.tests;
+
+import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.Selenide;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+
+import static com.codeborne.selenide.Browsers.CHROME;
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.*;
+
+public class PracticeFromTest {
+    @BeforeAll
+    static void beforeAll() {
+        Configuration.baseUrl = "https://demoqa.com";
+        Configuration.browserSize = "1920x1080";
+        Configuration.browser = CHROME;
+        Configuration.holdBrowserOpen = true;
+
+    }
+
+    @AfterAll
+    public static void afterAll(){
+        Selenide.closeWebDriver();
+    }
+
+    @Test
+    void SuccessTest(){
+
+        String firstName = "Angelina";
+        String lastName = "Savina";
+        String userEmail = "angelinaa@mail.com";
+        String userPhone = "1234567891";
+        String imageUrl = "imageTest.png";
+
+        open("/automation-practice-form");
+        executeJavaScript("$('footer').remove()");
+        executeJavaScript("$('#fixedban').remove()");
+
+        $("#firstName").setValue(firstName);
+        $("#lastName").setValue(lastName);
+        $("#userEmail").setValue(userEmail);
+        $(byText("Female")).click();
+        $("#userNumber").setValue(userPhone);
+        $("#dateOfBirth-wrapper").$("#dateOfBirthInput").click();
+        $("#dateOfBirth-wrapper").$(".react-datepicker__month-select").click();
+        $("#dateOfBirth-wrapper").$(byText("June")).click();
+        $("#dateOfBirth-wrapper").$(".react-datepicker__year-select").click();
+        $("#dateOfBirth-wrapper").$(byText("1996")).click();
+        $(".react-datepicker__day--008").click();
+        $("#subjectsInput").setValue("ec");
+        $(byText("Economics")).click();
+        $("#hobbiesWrapper").$(byText("Reading")).click();
+        $("#uploadPicture").uploadFromClasspath(imageUrl);
+        $("#currentAddress").setValue("Cyprus is current address");
+        $("#stateCity-wrapper").click();
+        $("#stateCity-wrapper").$(byText("NCR")).click();
+        $("#city").click();
+        $("#stateCity-wrapper").$(byText("Delhi")).click();
+        $("#submit").click();
+
+        $("#example-modal-sizes-title-lg").shouldHave(text("Thanks for submitting the form"));
+        $(".table-responsive").$(byText("Student Name")).parent().shouldHave(text("Angelina Savina"));
+        $(".table-responsive").$(byText("Student Email")).parent().shouldHave(text(userEmail));
+        $(".table-responsive").$(byText("Gender")).parent().shouldHave(text("Female"));
+        $(".table-responsive").$(byText("Mobile")).parent().shouldHave(text(userPhone));
+        $(".table-responsive").$(byText("Date of Birth")).parent().shouldHave(text("08 June,1996"));
+        $(".table-responsive").$(byText("Subjects")).parent().shouldHave(text("Economics"));
+        $(".table-responsive").$(byText("Hobbies")).parent().shouldHave(text("Reading"));
+        $(".table-responsive").$(byText("Picture")).parent().shouldHave(text("imageTest.png"));
+        $(".table-responsive").$(byText("Address")).parent().shouldHave(text("Cyprus is current address"));
+        $(".table-responsive").$(byText("State and City")).parent().shouldHave(text("NCR Delhi"));
+    }
+
+}
